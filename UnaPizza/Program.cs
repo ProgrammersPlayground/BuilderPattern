090ea47cdf946b25fbbfccef6d0af33a1f249f3a
ProgrammersPlayground <ProgrammersPlayground.de@gmail.com>
2024-11-24 18:43:07 +0100

    AddPizzaBuilder

---
+++ UnaPizza/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+using UnaPizza;
+
+Console.WriteLine("Willkommen beim PizzaBuilder!");
+Console.WriteLine("Wir erstellen eine Pizza Schritt für Schritt");
+Console.WriteLine("");
+
+//Erstellung der Pizza
+Pizza pizza = new PizzaBuilder()
+    .SetSize("Large")
+    .AddCheese()
+    .AddTomatoes()
+    .AddPepperoni()
+    .AddOlives()
+    .Build();
+
+//Ausgabe der fertigen Pizza
+Console.WriteLine("Ihre Pizza wurde erstellt:");
+Console.WriteLine($"Pizza [Size: {pizza.Size}, Cheese: {pizza.HasCheese}, Pepperoni: {pizza.HasPepperoni}, Tomatoes: {pizza.HasTomatoes}, Olives: {pizza.HasOlives}]");
+Console.WriteLine("");
+
+Console.WriteLine("Drücken Sie eine beliebige Taste um das Programm zu beenden");
+Console.ReadKey();
+
